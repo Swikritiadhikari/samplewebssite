5f3f34293c62e2c8c9e0dc61529dcf010c6a5df1
swikritiadhikari <sshushri1@gmail.com>
2022-03-22 04:23:19 -0400

    firstcommit

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <!-- top nav -->
+    <div class="bg-danger text-white">
+        <div class="container">
+            <h1> ITAHARI SUBMETROPOLITICIAN CITY OFFICE<h1>
+        </div>
+    </div>
+    <!--  -->
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">Home</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav meI-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Introduction</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Program</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Program and Project
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Budget and program</a></li>
+                                <li><a class="dropdown-item" href="#">Budget</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Plan and project</a></li>
+                            </ul>
+
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- banner -->
+        <div class="d-flex justify-content-center align-items-sm-center" style="background-image: url('images/n.jpeg'); width:100%; min-height:60vh;background-position:center">
+            <div>
+                <h1 class="bg-danger px-4 text-white-50 rounded text-uppercase"> Itahari <span style="color:darkmagenta;">sunsari</span> nepal,Inaruwa</h1>
+            </div>
+
+
+
+
+        </div class>
+        <!-- Introduction -->
+      <div class="py-5"><div class="container">
+          <div class="row">
+              <div class="col">
+                  <div class="col md-3"></div>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam possimus consequuntur labore accusantium doloribus maxime atque amet. Modi, praesentium consectetur. Atque officiis laborum vero soluta ab mollitia quia aperiam fuga error consequuntur nobis doloribus vitae dolorum sunt earum nihil, eaque perspiciatis, deleniti quo obcaecati cumque incidunt sint ducimus quibusdam? Perferendis molestiae magnam laboriosam quasi nostrum obcaecati recusandae corporis ducimus exercitationem quos expedita id, similique minima libero est, eius possimus ad excepturi voluptates quod suscipit modi. Nam magni molestiae sunt. Provident cumque quae numquam sunt recusandae rem assumenda commodi ullam deleniti impedit sit laborum natus, laudantium perferendis, perspiciatis aliquid aperiam maxime tenetur rerum culpa modi cupiditate. Facilis, ipsam obcaecati. Adipisci esse ad numquam. Officiis dolore fuga in eum praesentium illum tempora obcaecati nesciunt magnam architecto, aliquam dolorem? Consectetur natus, dolores laudantium dolorum minima deleniti? Illo consequuntur facilis tempora incidunt id magni delectus deleniti, voluptates repellat dolorem dicta, aspernatur cumque minus eos ex minima quisquam! Commodi animi quas eligendi sit incidunt quos dolorem error! Accusamus exercitationem tempora, laboriosam praesentium quasi odit? Voluptatibus cumque eos beatae laborum ex repellat magnam amet? Vel similique debitis reprehenderit vero amet quos ut, alias aliquam culpa officiis porro quibusdam corrupti nulla, cupiditate repellendus laudantium saepe id maxime laborum, inventore harum eos minus molestias ipsa. Laudantium aliquam quibusdam magnam suscipit libero! Corporis, incidunt ipsam harum doloribus impedit fugit.</p>
+              </div>
+              <div class="col">
+                  <div class="col md-3"></div>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam possimus consequuntur labore accusantium doloribus maxime atque amet. Modi, praesentium consectetur. Atque officiis laborum vero soluta ab mollitia quia aperiam fuga error consequuntur nobis doloribus vitae dolorum sunt earum nihil, eaque perspiciatis, deleniti quo obcaecati cumque incidunt sint ducimus quibusdam? Perferendis molestiae magnam laboriosam quasi nostrum obcaecati recusandae corporis ducimus exercitationem quos expedita id, similique minima libero est, eius possimus ad excepturi voluptates quod suscipit modi. Nam magni molestiae sunt. Provident cumque quae numquam sunt recusandae rem assumenda commodi ullam deleniti impedit sit laborum natus, laudantium perferendis, perspiciatis aliquid aperiam maxime tenetur rerum culpa modi cupiditate. Facilis, ipsam obcaecati. Adipisci esse ad numquam. Officiis dolore fuga in eum praesentium illum tempora obcaecati nesciunt magnam architecto, aliquam dolorem? Consectetur natus, dolores laudantium dolorum minima deleniti? Illo consequuntur facilis tempora incidunt id magni delectus deleniti, voluptates repellat dolorem dicta, aspernatur cumque minus eos ex minima quisquam! Commodi animi quas eligendi sit incidunt quos dolorem error! Accusamus exercitationem tempora, laboriosam praesentium quasi odit? Voluptatibus cumque eos beatae laborum ex repellat magnam amet? Vel similique debitis reprehenderit vero amet quos ut, alias aliquam culpa officiis porro quibusdam corrupti nulla, cupiditate repellendus laudantium saepe id maxime laborum, inventore harum eos minus molestias ipsa. Laudantium aliquam quibusdam magnam suscipit libero! Corporis, incidunt ipsam harum doloribus impedit fugit.</p>
+              </div>
+              <div class="col">
+                  <div class="col md-3"></div>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam possimus consequuntur labore accusantium doloribus maxime atque amet. Modi, praesentium consectetur. Atque officiis laborum vero soluta ab mollitia quia aperiam fuga error consequuntur nobis doloribus vitae dolorum sunt earum nihil, eaque perspiciatis, deleniti quo obcaecati cumque incidunt sint ducimus quibusdam? Perferendis molestiae magnam laboriosam quasi nostrum obcaecati recusandae corporis ducimus exercitationem quos expedita id, similique minima libero est, eius possimus ad excepturi voluptates quod suscipit modi. Nam magni molestiae sunt. Provident cumque quae numquam sunt recusandae rem assumenda commodi ullam deleniti impedit sit laborum natus, laudantium perferendis, perspiciatis aliquid aperiam maxime tenetur rerum culpa modi cupiditate. Facilis, ipsam obcaecati. Adipisci esse ad numquam. Officiis dolore fuga in eum praesentium illum tempora obcaecati nesciunt magnam architecto, aliquam dolorem? Consectetur natus, dolores laudantium dolorum minima deleniti? Illo consequuntur facilis tempora incidunt id magni delectus deleniti, voluptates repellat dolorem dicta, aspernatur cumque minus eos ex minima quisquam! Commodi animi quas eligendi sit incidunt quos dolorem error! Accusamus exercitationem tempora, laboriosam praesentium quasi odit? Voluptatibus cumque eos beatae laborum ex repellat magnam amet? Vel similique debitis reprehenderit vero amet quos ut, alias aliquam culpa officiis porro quibusdam corrupti nulla, cupiditate repellendus laudantium saepe id maxime laborum, inventore harum eos minus molestias ipsa. Laudantium aliquam quibusdam magnam suscipit libero! Corporis, incidunt ipsam harum doloribus impedit fugit.</p>
+              </div>
+              <div class="col">
+                  <div class="col md-3"></div>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam possimus consequuntur labore accusantium doloribus maxime atque amet. Modi, praesentium consectetur. Atque officiis laborum vero soluta ab mollitia quia aperiam fuga error consequuntur nobis doloribus vitae dolorum sunt earum nihil, eaque perspiciatis, deleniti quo obcaecati cumque incidunt sint ducimus quibusdam? Perferendis molestiae magnam laboriosam quasi nostrum obcaecati recusandae corporis ducimus exercitationem quos expedita id, similique minima libero est, eius possimus ad excepturi voluptates quod suscipit modi. Nam magni molestiae sunt. Provident cumque quae numquam sunt recusandae rem assumenda commodi ullam deleniti impedit sit laborum natus, laudantium perferendis, perspiciatis aliquid aperiam maxime tenetur rerum culpa modi cupiditate. Facilis, ipsam obcaecati. Adipisci esse ad numquam. Officiis dolore fuga in eum praesentium illum tempora obcaecati nesciunt magnam architecto, aliquam dolorem? Consectetur natus, dolores laudantium dolorum minima deleniti? Illo consequuntur facilis tempora incidunt id magni delectus deleniti, voluptates repellat dolorem dicta, aspernatur cumque minus eos ex minima quisquam! Commodi animi quas eligendi sit incidunt quos dolorem error! Accusamus exercitationem tempora, laboriosam praesentium quasi odit? Voluptatibus cumque eos beatae laborum ex repellat magnam amet? Vel similique debitis reprehenderit vero amet quos ut, alias aliquam culpa officiis porro quibusdam corrupti nulla, cupiditate repellendus laudantium saepe id maxime laborum, inventore harum eos minus molestias ipsa. Laudantium aliquam quibusdam magnam suscipit libero! Corporis, incidunt ipsam harum doloribus impedit fugit.</p>
+              </div>
+          </div>
+      </div></div>
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+    <!-- js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
+</html>
